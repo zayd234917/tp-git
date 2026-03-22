3664df50b190c93aa04cab1c7f53c8a4d95ef816
zayd234917 <zaidbenkhaira@gmail.com>
2026-03-22 16:43:38 +0100

    adding new files in the branch dev

---
+++ main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public Class Main{
+    public static void main(){
+        Scanner scn = new Scanner(system.in)
+         string input;
+         System.out.println("enter une message");
+         input = 
+    }
+}
