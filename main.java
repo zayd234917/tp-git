e2977cc9da3c7e25535cd48558c11925a903a446
zayd234917 <zaidbenkhaira@gmail.com>
2026-03-25 17:47:51 +0000

    restart

--- main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public Class Main{
-    public static void main(){
-        Scanner scn = new Scanner(system.in)
-         string input;
-         System.out.println("enter une message");
-         input = 
-    }
-}
--- test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(){
-    int a = 10 ;
-    int b = 20;
-     printf(a+b);
-    return 0;
-}
